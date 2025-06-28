6ac5d464c6e20c3cf672194182da31b20723f1e8
jronceros80 <jronceros80@gmail.com>
2025-06-28 11:32:53 +0200

    feat: create class WebConfig to accept request CORS

---
+++ src/main/java/com/products/infrastructure/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.products.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600); // 1 hour cache to response pre-flight
+    }
+}
